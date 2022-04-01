26118d665bb06d7a31ff864e9c4e5d1162492bbc
Adam Nowland <acnowland@gmail.com>
2022-04-01 13:28:43 -0700

    Initial Commit

---
+++ FlagGuess/FlagGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessApp.swift
+//  FlagGuess
+//
+//  Created by Adam Nowland on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
